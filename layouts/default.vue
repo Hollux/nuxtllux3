40b91af4b0b94b139df4c1926b1b83ac8500d50a
hollux@hotmail.fr <hollux@hotmail.fr>
2020-06-22 11:15:10 +0200

    up infos ludodrive + page contact

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <navbar />
-    <nuxt />
+    <div class="content">
+      <nuxt />
+    </div>
     <footerhlx />
   </div>
 </template>
@@ -19,55 +21,55 @@ export default {
 </script>
 
 <style>
-  *,
-  *:before,
-  *:after {
-    box-sizing: border-box;
-    margin: 0;
-  }
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
 
-  .button--green {
-    display: inline-block;
-    border-radius: 4px;
-    border: 1px solid #3b8070;
-    color: #3b8070;
-    text-decoration: none;
-    padding: 10px 30px;
-  }
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
+}
 
-  .button--green:hover {
-    color: #fff;
-    background-color: #3b8070;
-  }
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
 
-  .button--grey {
-    display: inline-block;
-    border-radius: 4px;
-    border: 1px solid #35495e;
-    color: #35495e;
-    text-decoration: none;
-    padding: 10px 30px;
-    margin-left: 15px;
-  }
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
 
-  .button--grey:hover {
-    color: #fff;
-    background-color: #35495e;
-  }
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
 
-  a {
-    outline: 0 !important;
-    color: black;
-  }
-  .container {
-    display: block !important;
-    text-align: left !important;
-    min-height: 0px !important;
-  }
-  .center {
-    text-align: center;
-  }
-  .footernavhlx {
-    background-color: black;
-  }
+a {
+  outline: 0 !important;
+  color: black;
+}
+.container {
+  display: block !important;
+  text-align: left !important;
+  min-height: 0px !important;
+}
+.center {
+  text-align: center;
+}
+.footernavhlx {
+  background-color: black;
+}
 </style>
