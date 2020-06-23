5df5ff39039bce14dcb922ad39c9072b3e39b43f
hollux@hotmail.fr <hollux@hotmail.fr>
2020-06-23 14:12:10 +0200

    debug, favicon, lien CV

---
+++ components/miseEnForme/buttonTraitExterne.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="buttonTrait">
+    <div class="test">
+      <hr color="black" />
+      <a :href="link" target="_blank">{{ linkName }}</a>
+      <hr color="black" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    link: {
+      type: String,
+      default: "/"
+    },
+    linkName: {
+      type: String,
+      default: "link"
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.test {
+  display: flex;
+  margin: 35px 0;
+  hr {
+    width: 100%;
+    color: black;
+    margin: 26px 0;
+  }
+  a {
+    color: white;
+    background-color: black;
+    padding: 11px 35px;
+    margin: 5px 45px;
+    white-space: nowrap;
+    font-family: "raleway", sans-serif;
+    text-transform: uppercase;
+    font-size: 0.85rem;
+    font-style: normal;
+    font-weight: 700;
+  }
+}
+</style>
